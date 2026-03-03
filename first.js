417a4d3dbb8e34bc87ea47a8011bc454682298c4
Anshika rajpoot <anshikarajpoot1570@gmail.com>
2026-03-03 21:47:27 +0530

    Revert " third commmit"
    This reverts commit 2d0865fe67486d38fd7d20feb14aafbd616f7cae.

--- first.js
+++ first.js
@@ -2,9 +2,13 @@ const  name="rohit"
 const age=20;
  const balance=300000;
 
+<<<<<<< HEAD
   function update(){
      balance+=300;
   }
   update();
   console.log("hello coder army")
 
+=======
+  const account_no=234456
+>>>>>>> parent of 2d0865f ( third commmit)
